acde8c77446efdb351a744c65ffa883360084260
Derui Deng <drdeng@thoughtworks.com>
2017-07-19 11:35:01 +0800

    持久化层的查询，添加，删除卡片功能

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,16 +1,24 @@
 import { isCardValid } from './card';
 
-const readCards = () => [];
+const readCards = () => {
+  const cardsStringInStorage = window.localStorage.getItem('SessionWallCards');
+  return cardsStringInStorage ? JSON.parse(cardsStringInStorage) : [];
+};
 
 const persistCard = (card) => {
   if (isCardValid(card)) {
-    let cardsInStorage = localStorage.getItem('SessionWallCards') || [];
+    let cardsInStorage = readCards();
+
     cardsInStorage = cardsInStorage.filter(cardInStorage => cardInStorage.id !== card.id);
     cardsInStorage.push(card);
-    localStorage.setItem('SessionWallCards', cardsInStorage);
+    window.localStorage.setItem('SessionWallCards', JSON.stringify(cardsInStorage));
   }
 };
 
-const removeCard = cardId => cardId;
+const removeCard = (cardId) => {
+  let cardsInStorage = readCards();
+  cardsInStorage = cardsInStorage.filter(cardInStorage => cardInStorage.id !== cardId);
+  window.localStorage.setItem('SessionWallCards', JSON.stringify(cardsInStorage));
+};
 
 export default { readCards, persistCard, removeCard };
